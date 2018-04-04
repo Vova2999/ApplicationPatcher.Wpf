99701b0c697f614168a23016b48e3a98f1151135
Vova2999 <Vova2999@gmail.com>
2018-04-04 14:36:07 +0400

    Добавил nuget пакеты и зависимости

---
+++ .rebuild-ApplicationPatcher.bat
@@ -0,0 +1,6 @@
+@echo off
+pushd %~dp0
+call ..\ApplicationPatcher\.rebuild-release.bat
+call ..\ApplicationPatcher\Build\ApplicationPatcher.Self\ApplicationPatcher.Self.exe
+popd
+@echo on
--- scripts/build/build-all-solutions.bat
+++ scripts/build/build-all-solutions.bat
@@ -8,6 +8,6 @@ call build-solution.bat ..\..\_source\ApplicationPatcher.Wpf.sln %1
 tasklist | find "MSBuild.exe" > nul
 if %errorLevel%==0 taskkill /IM MSBuild.exe /f
 
-if %1==Release del /S /Q ..\Build\*.pdb > nul
+if %1==Release del /S /Q ..\..\Build\*.pdb > nul
 
 popd
